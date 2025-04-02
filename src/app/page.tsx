e32911472c1471cbe45e9c900887ca07e04a4321
NeilRow56 <neilrowland56@gmail.com>
2025-04-02 05:50:42 +0100

    Register form - styling added. Submit giving alert data correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { SignUp } from "@/forms/sign-up";
+import { RegisterForm } from "@/forms/sign-up-advanced";
 import TanstackFormExample from "@/forms/tanstack-example.form";
 
 export default async function Home() {
   return (
     <div className="  ">
       {/* <TanstackFormExample /> */}
-      <SignUp />
+      <RegisterForm />
     </div>
   );
 }
--- src/forms/sign-up-advanced.tsx
+++ src/forms/sign-up-advanced.tsx
@@ -48,119 +48,135 @@ export const RegisterForm: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Register</h1>
-      {/* Username */}
-      <div>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-        />
-      </div>
-      {/* Email */}
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-      </div>
-      {/* First Name */}
-      <div>
-        <label htmlFor="firstName">FirstName</label>
-        <input
-          type="text"
-          id="firstName"
-          name="firstName"
-          value={formData.firstName}
-          onChange={handleChange}
-        />
-      </div>
-      {/* Last Name */}
-      <div>
-        <label htmlFor="lastName">LastName</label>
-        <input
-          type="text"
-          id="lastName"
-          name="lastName"
-          value={formData.lastName}
-          onChange={handleChange}
-        />
-      </div>
+    <div className="flex flex-col h-full w-[600px] container mx-auto mt-24 ">
+      <form className="space-y-4 " onSubmit={handleSubmit}>
+        <div>
+          <h1 className="text-2xl font-bold">Register</h1>
+        </div>
 
-      {/* Birthdate */}
-      <div>
-        <label htmlFor="birthdate">Birthdate</label>
-        <input
-          type="date"
-          id="birthdate"
-          name="birthdate"
-          value={formData.birthdate}
-          onChange={handleChange}
-        />
-      </div>
-
-      {/* is Married */}
-      <div>
-        <label htmlFor="isMarried">
+        {/* Username */}
+        <div className="flex flex-col mb-2">
+          <label htmlFor="username">Username</label>
           <input
-            type="checkbox"
-            id="isMarried"
-            name="isMarried"
-            checked={formData.isMarried}
+            className="border "
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
             onChange={handleChange}
           />
-          Are you married?
-        </label>
-      </div>
+        </div>
+        {/* Email */}
+        <div className="flex flex-col mb-2">
+          <label htmlFor="email">Email</label>
+          <input
+            className="border "
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        {/* First Name */}
+        <div className="flex flex-col mb-2">
+          <label htmlFor="firstName">FirstName</label>
+          <input
+            className="border "
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </div>
+        {/* Last Name */}
+        <div className="flex flex-col mb-2">
+          <label htmlFor="lastName">LastName</label>
+          <input
+            className="border "
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+        </div>
 
-      {/* Nationality */}
-      <div>
-        <label htmlFor="isMarried"></label>
-        <select
-          id="nationality"
-          name="nationality"
-          value={formData.nationality}
-          onChange={handleChange}
-        >
-          <option value="canada">Canada</option>
-          <option value="usa">USA</option>
-          <option value="india">India</option>
-          <option value="brazil">Brazil</option>
-        </select>
-      </div>
+        {/* Birthdate */}
+        <div className="flex flex-col mb-2">
+          <label htmlFor="birthdate">Birthdate</label>
+          <input
+            className="border "
+            type="date"
+            id="birthdate"
+            name="birthdate"
+            value={formData.birthdate}
+            onChange={handleChange}
+          />
+        </div>
 
-      {/* Password */}
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="text"
-          id="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-      </div>
+        {/* is Married */}
+        <div className="flex flex-col mb-2">
+          <label htmlFor="isMarried">
+            <input
+              className="border "
+              type="checkbox"
+              id="isMarried"
+              name="isMarried"
+              checked={formData.isMarried}
+              onChange={handleChange}
+            />
+            Are you married?
+          </label>
+        </div>
 
-      {/* Confirm Password */}
-      <div>
-        <label htmlFor="confirm_password"> Confirm Password</label>
-        <input
-          type="text"
-          id="confirm_password"
-          name="confirm_password"
-          value={formData.confirm_password}
-          onChange={handleChange}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+        {/* Nationality */}
+        <div className="flex flex-col mb-2">
+          <label htmlFor="isMarried"></label>
+          <select
+            className="border "
+            id="nationality"
+            name="nationality"
+            value={formData.nationality}
+            onChange={handleChange}
+          >
+            <option value="canada">Canada</option>
+            <option value="usa">USA</option>
+            <option value="india">India</option>
+            <option value="brazil">Brazil</option>
+          </select>
+        </div>
+
+        {/* Password */}
+        <div className="flex flex-col mb-2">
+          <label htmlFor="password">Password</label>
+          <input
+            className="border "
+            type="text"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+
+        {/* Confirm Password */}
+        <div className="flex flex-col mb-2">
+          <label htmlFor="confirm_password"> Confirm Password</label>
+          <input
+            className="border "
+            type="text"
+            id="confirm_password"
+            name="confirm_password"
+            value={formData.confirm_password}
+            onChange={handleChange}
+          />
+        </div>
+        <button className="bg-blue-500 text-white rounded-lg p-2" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
